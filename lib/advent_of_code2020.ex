e0c3995a11fd86db85f8362cce5db654b70d0358
Philip Sampaio Silva <philip.sampaio@gmail.com>
2020-12-06 01:18:49 -0300

    Solve day 4 and 5

--- lib/advent_of_code2020.ex
+++ lib/advent_of_code2020.ex
@@ -175,4 +175,171 @@ defmodule AdventOfCode2020 do
         trees * total
     end)
   end
+
+  @required_passport_fields ~w(byr iyr eyr hgt hcl ecl pid)
+  @doc """
+  Passaport validator.
+
+  ## Examples
+
+      iex> passport = ~s(ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+      iex> byr:1937 iyr:2017 cid:147 hgt:183cm)
+      iex> AdventOfCode2020.passport_valid?(passport)
+      true
+
+      iex> passport = ~s(hcl:#ae17e1 iyr:2013
+      iex> eyr:2024
+      iex> ecl:brn pid:760753108 byr:1931
+      iex> hgt:179cm)
+      iex> AdventOfCode2020.passport_valid?(passport)
+      true
+
+      iex> passport = ~s(
+      iex> iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+      iex> hcl:#cfa07d byr:1929)
+      iex> AdventOfCode2020.passport_valid?(passport)
+      false
+
+
+  """
+  def passport_valid?(passport_fields) do
+    pairs = String.split(passport_fields, ~r/[\s\n]/, trim: true)
+
+    passport =
+      for pair <- pairs, into: %{} do
+        [key, value] = String.split(pair, ":", trim: true)
+        {key, value}
+      end
+
+    @required_passport_fields -- Map.keys(passport) == []
+  end
+
+  @doc """
+  Validate passport according to some rules.
+
+  The rules are:
+
+  - byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  - iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  - eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  - hgt (Height) - a number followed by either cm or in:
+    - If cm, the number must be at least 150 and at most 193.
+    - If in, the number must be at least 59 and at most 76.
+  - hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  - ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  - pid (Passport ID) - a nine-digit number, including leading zeroes.
+  - cid (Country ID) - ignored, missing or not.
+
+  ## Examples
+
+      iex> passport = ~s(pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f)
+      iex> AdventOfCode2020.passport_valid_with_rules?(passport)
+      true
+
+      iex> passport = ~s(eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926)
+      iex> AdventOfCode2020.passport_valid_with_rules?(passport)
+      false
+
+  """
+  def passport_valid_with_rules?(passport_fields) do
+    pairs = String.split(passport_fields, ~r/[\s\n]/, trim: true)
+
+    fields =
+      for pair <- pairs do
+        [key, value] = String.split(pair, ":", trim: true)
+        {key, value}
+      end
+
+    Enum.all?(fields, &valid_passport_field?(&1))
+  end
+
+  defp valid_passport_field?({"byr", year}) do
+    parse_and_validate_integer_in_range(year, 1920..2002)
+  end
+
+  defp valid_passport_field?({"iyr", year}) do
+    parse_and_validate_integer_in_range(year, 2010..2020)
+  end
+
+  defp valid_passport_field?({"eyr", expiration_year}) do
+    parse_and_validate_integer_in_range(expiration_year, 2020..2030)
+  end
+
+  defp valid_passport_field?({"hgt", height}) do
+    case Integer.parse(height) do
+      {number, "cm"} ->
+        number in 150..193
+
+      {number, "in"} ->
+        number in 59..76
+
+      _ ->
+        false
+    end
+  end
+
+  defp valid_passport_field?({"hcl", color}) do
+    String.match?(color, ~r/^#[a-f0-9]{6}$/)
+  end
+
+  defp valid_passport_field?({"ecl", eye_color})
+       when eye_color in ~w(amb blu brn gry grn hzl oth) do
+    true
+  end
+
+  defp valid_passport_field?({"pid", passport_id}) do
+    String.match?(passport_id, ~r/^[0-9]{9}$/)
+  end
+
+  defp valid_passport_field?({"cid", _}), do: true
+
+  defp valid_passport_field?(_), do: false
+
+  defp parse_and_validate_integer_in_range(value, range) do
+    case Integer.parse(value) do
+      {number, ""} ->
+        number in range
+
+      _ ->
+        false
+    end
+  end
+
+  @doc """
+  Searches for the seat id by looking to nearby passes.
+
+  ## Example
+
+      iex> AdventOfCode2020.binary_boarding("FBFBBFFRLR")
+      357
+
+      iex> AdventOfCode2020.binary_boarding("BFFFBBFRRR")
+      567
+
+  """
+  def binary_boarding(binary_space_partioning) do
+    coords = String.codepoints(binary_space_partioning)
+    rows_coords = Enum.take(coords, 7)
+    cols_coords = Enum.take(coords, -3)
+
+    [row] = binary_boarding_search(rows_coords, Enum.to_list(0..127))
+    [col] = binary_boarding_search(cols_coords, Enum.to_list(0..7))
+
+    row * 8 + col
+  end
+
+  defp binary_boarding_search(coords, seats) do
+    Enum.reduce(coords, seats, fn coord, remaining ->
+      split_by = Integer.floor_div(length(remaining), 2)
+
+      to_take =
+        if coord in ~w(F L) do
+          split_by
+        else
+          -1 * split_by
+        end
+
+      Enum.take(remaining, to_take)
+    end)
+  end
 end
